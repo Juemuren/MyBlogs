59bf52c915d55dda75a186304a3bc8b91a9d854f
Raind <127611616+Juemuren@users.noreply.github.com>
2026-04-03 20:12:06 +0800

    script: 修改知乎导出脚本
    - 处理注释，使其原样输出

--- scripts/shift-headers.lua
+++ scripts/shift-headers.lua
@@ -1,10 +1,23 @@
+-- Decreasing Heading Level
 function Header(el)
-  -- 删除一级标题
   if el.level == 1 then
     return {}
   else
-    -- 其它标题级别减一
     el.level = el.level - 1
     return el
+  end
+end
+
+-- Keep Inline Comments
+function RawInline(el)
+  if el.format == "html" and el.text:match("^<!%-%-.*%-%->$") then
+    return pandoc.RawInline("markdown", el.text)
+  end
+end
+
+-- Keep Block Comments
+function RawBlock(el)
+  if el.format == "html" and el.text:match("^<!%-%-.*%-%->$") then
+    return pandoc.RawBlock("markdown", el.text)
   end
 end
