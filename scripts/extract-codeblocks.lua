c96f2293473dbd9ace2c0baea7a6bd505d52da7b
Raind <127611616+Juemuren@users.noreply.github.com>
2026-07-16 01:14:01 +0800

    scripts: 处理 jsxgraph 和 abc 的代码块的单文件导出

--- scripts/extract-codeblocks.lua
+++ scripts/extract-codeblocks.lua
@@ -1,35 +1,65 @@
-local file = PANDOC_STATE.input_files[1]
-local path_stem = pandoc.path.split_extension(file)
-local stem = pandoc.path.filename(path_stem)
+local input_file = PANDOC_STATE.input_files[1]
+local input_path_stem = pandoc.path.split_extension(input_file)
+local input_stem = pandoc.path.filename(input_path_stem)
 
-local counters = { tikz = 0, mermaid = 0 }
+local diagram_counts = {
+  tikz = 0,
+  mermaid = 0,
+}
 
-local function replace_codeblock(key, ext, content)
-  counters[key] = counters[key] + 1
-  local filename = string.format("%s-%s-%d.%s", path_stem, key, counters[key], ext)
-  pandoc.system.write_file(filename, content)
-  local caption = string.format("%s-%s-%d", stem, key, counters[key])
-  local path = string.format("%s-%s-%d.svg", stem, key, counters[key])
-  local img = pandoc.Image({pandoc.Str(caption)}, path)
-  return pandoc.Para({img})
-end
-
--- Extract Codeblock into Standalone
-function CodeBlock(el)
-  local class = el.classes and el.classes[1]
-  if class == "tikz" then
-    local pre = [[
+local TIKZ_PREAMBLE = [[
 \documentclass[tikz]{standalone}
 \usepackage{tikz}
 \begin{document}
 ]]
-    local post = [[
+
+local TIKZ_POSTAMBLE = [[
 \end{document}
 ]]
-    return replace_codeblock("tikz", "tex", pre .. el.text .. post)
-  elseif class == "mermaid" then
-    return replace_codeblock("mermaid", "mmd", el.text)
-  else
-    return nil
+
+local function next_diagram_suffix(kind)
+  diagram_counts[kind] = diagram_counts[kind] + 1
+  return string.format("%s-%d", kind, diagram_counts[kind])
+end
+
+local function extract_codeblock(kind, extension, source)
+  local diagram_suffix = next_diagram_suffix(kind)
+  local source_path = string.format("%s-%s.%s", input_path_stem, diagram_suffix, extension)
+  local diagram_name = string.format("%s-%s", input_stem, diagram_suffix)
+  local diagram_path = diagram_name .. ".svg"
+
+  pandoc.system.write_file(source_path, source)
+
+  local diagram = pandoc.Image({ pandoc.Str(diagram_name) }, diagram_path)
+  return pandoc.Para({ diagram })
+end
+
+local codeblock_handlers = {
+  tikz = function(codeblock)
+    local source = TIKZ_PREAMBLE .. codeblock.text .. TIKZ_POSTAMBLE
+    return extract_codeblock("tikz", "tex", source)
+  end,
+
+  mermaid = function(codeblock)
+    return extract_codeblock("mermaid", "mmd", codeblock.text)
+  end,
+
+  jsxgraph = function()
+    return {}
+  end,
+
+  abc = function()
+    return {}
+  end,
+}
+
+function CodeBlock(codeblock)
+  for _, class in ipairs(codeblock.classes) do
+    local handler = codeblock_handlers[class]
+    if handler then
+      return handler(codeblock)
+    end
   end
+
+  return nil
 end
